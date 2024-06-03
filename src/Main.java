6cb360fcdecee371ce45c5eca5b7799876dbb584
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-06-03 12:10:03 +0900

    html불러와서 원하는곳만 가져오기

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -9,19 +7,28 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        String address = "http://www.codechobo.com/post/notice/list";
-        String line= "";
-        BufferedReader in = null;
+        String address = "https://news.daum.net/?nil_profile=mini&nil_src=news";
+
+        BufferedReader br = null;
         URL url = null;
 
         try {
             url = new URL(address);
-            in = new BufferedReader(new InputStreamReader(url.openStream()));
-
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
+            br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line  = "";
+            boolean flag = false;
+            while ((line = br.readLine()) != null) {
+                if(line.contains("<ul class=\"list_newsissue\">")){
+                    flag = true;
+                    continue;
+                }
+                if(line.contains("</ul>")){
+                    flag = false;
+                }
+                if(flag){
+                    System.out.println(line);
+                }
             }
-            in.close();
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
