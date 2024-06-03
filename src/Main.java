fde0e3149ed311aa1df1c6a7efb03b4f32fb8908
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-06-03 11:16:37 +0900

    html 불러오기

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -6,13 +9,23 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        String urlStr = "http://www.codechobo.com:80/main";
+        String address = "http://www.codechobo.com/post/notice/list";
+        String line= "";
+        BufferedReader in = null;
+        URL url = null;
 
         try {
-            URL url = new URL(urlStr);
-            System.out.println(url.getProtocol());
-            System.out.println(url.getHost());
-            System.out.println(url.getPort());
-        }catch (MalformedURLException e){}
+            url = new URL(address);
+            in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
