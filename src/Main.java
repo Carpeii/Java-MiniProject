2ba9944d6b42fa1141bc066ff77d40cf1baac6b6
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-06-03 10:58:44 +0900

    java.net.URL 메소드

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "http://www.codechobo.com:80/main";
+        String urlStr = "http://www.codechobo.com:80/main";
 
-        //프로토콜 : http
-        //호스트명 : www.codechobo.com
-        //포트명 : 80
-        //StringTokenizer(url, ":/") 코드는 문자열에서 콜론(:) 또는 슬래시(/)가 나올때마다 해당 부분을 기준으로 문자열을 나눕니다. 이렇게 나눈 문자열들을 토큰이라고 부릅니다.
-
-        StringTokenizer st = new StringTokenizer(url, ":/");
-        while (st.hasMoreTokens()) {
-            System.out.println(st.nextToken());
-        }
+        try {
+            URL url = new URL(urlStr);
+            System.out.println(url.getProtocol());
+            System.out.println(url.getHost());
+            System.out.println(url.getPort());
+        }catch (MalformedURLException e){}
     }
 }
