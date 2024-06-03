0426f354e655925b167a5007c0acfac59445b0e5
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-06-03 14:50:54 +0900

    이미지 추가하기

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,30 @@
 import java.io.*;
-import java.net.InetAddress;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        String address = "https://news.daum.net/?nil_profile=mini&nil_src=news";
-
-        BufferedReader br = null;
+        String address = "https://m.daum.net";
+        URLConnection con = null;
+        String imgAdd = "https://img1.daumcdn.net/thumb/S192x120ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202406%2F03%2Finews24%2F20240603114955087fevu.jpg&scode=media2";
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
         URL url = null;
 
+
         try {
-            url = new URL(address);
-            br = new BufferedReader(new InputStreamReader(url.openStream()));
-            String line  = "";
-            boolean flag = false;
-            boolean flag2 = false;
-            while ((line = br.readLine()) != null) {
-                if(line.contains("class=\"link_txt\" data-tiara-layer=\"article_main\"")){
-                    flag = true;
-                    continue;
-                }
-                if(line.contains("</a>")){
-                    flag = false;
-                }
-                if(flag){
-                    System.out.println(line.trim());
-                }
+            url = new URL(imgAdd);
+            con = url.openConnection();
+
+            bis = new BufferedInputStream(con.getInputStream());
+            bos = new BufferedOutputStream(new FileOutputStream("./image.jpg"));
+
+            int data = 0;
+            while ((data = bis.read()) != -1) {
+                bos.write(data);
             }
+            System.out.println("이미지전송완");
+
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
