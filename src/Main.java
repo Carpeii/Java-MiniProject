da5a5d8b653804f38fee386e57fc87b644e7cdb8
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-06-03 10:56:31 +0900

    url 스트링 분리 StringTokenizer 로

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            InetAddress inetAddress = InetAddress.getByName("www.daum.net");
-            System.out.println(inetAddress.getHostName());
-            System.out.println(inetAddress.getHostAddress());
+        String url = "http://www.codechobo.com:80/main";
 
-            InetAddress[] inetAddresses = InetAddress.getAllByName("www.naver.net");
-            for (InetAddress inetAddress1 : inetAddresses) {
-                System.out.println(inetAddress1.getHostAddress());
-            }
-        }catch (UnknownHostException e){
-            e.printStackTrace();
+        //프로토콜 : http
+        //호스트명 : www.codechobo.com
+        //포트명 : 80
+        //StringTokenizer(url, ":/") 코드는 문자열에서 콜론(:) 또는 슬래시(/)가 나올때마다 해당 부분을 기준으로 문자열을 나눕니다. 이렇게 나눈 문자열들을 토큰이라고 부릅니다.
+
+        StringTokenizer st = new StringTokenizer(url, ":/");
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
         }
     }
 }
