b82b7128dbccc0ad1f79b01af3c8cba72c4c66ca
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-06-03 10:46:02 +0900

    ip 보기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            InetAddress inetAddress = InetAddress.getByName("www.daum.net");
+            System.out.println(inetAddress.getHostName());
+            System.out.println(inetAddress.getHostAddress());
+
+            InetAddress[] inetAddresses = InetAddress.getAllByName("www.naver.net");
+            for (InetAddress inetAddress1 : inetAddresses) {
+                System.out.println(inetAddress1.getHostAddress());
+            }
+        }catch (UnknownHostException e){
+            e.printStackTrace();
+        }
     }
 }
