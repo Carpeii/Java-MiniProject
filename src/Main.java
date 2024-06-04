36d883b7e9c39991d8e36de1832ea1236459cd81
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-06-04 11:31:31 +0900

    xml reading

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,47 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
-import java.net.*;
-import java.util.StringTokenizer;
 
 public class Main {
+    public static StringBuilder readFile(String fileName) {
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            String line = null;
+            while ((line = br.readLine())!= null){
+                sb.append(line+System.lineSeparator());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sb;
+    }
     public static void main(String[] args){
-        String strUrl = "https://news.daum.net/?nil_profile=mini&nil_src=news";
+        StringBuilder sb = readFile("./data.xml");
 
-//        Document doc = Jsoup.connect();
+        Document doc = Jsoup.parse(sb.toString());
 
-//        System.out.println(doc);
-//        System.out.println(doc.title());
-//        System.out.println(body);
-//        System.out.println(doc.charset());
+        Elements names = doc.getElementsByTag("book");
+        Elements titles = doc.getElementsByTag("title");
+        Elements authors = doc.getElementsByTag("author");
+        Elements years = doc.getElementsByTag("year");
+        Elements prices = doc.getElementsByTag("price");
+
+        for (int i = 0; i < names.size(); i++) {
+            Element name = names.get(i);
+            Element title = titles.get(i);
+            Element author = authors.get(i);
+            Element year = years.get(i);
+            Element price = prices.get(i);
+
+            System.out.println(name.text());
+            System.out.println(title.text());
+            System.out.println(author.text());
+            System.out.println(year.text());
+            System.out.println(price.text());
+        }
     }
 }
